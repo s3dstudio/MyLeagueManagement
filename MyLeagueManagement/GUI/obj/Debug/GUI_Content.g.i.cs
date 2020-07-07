7d320026fbf44bd168fd77ac4186abf0bf7fe79e
SangMinnh <angelmaster49@gmail.com>
2020-07-07 08:59:01 +0700

    Update some Image

--- MyLeagueManagement/GUI/obj/Debug/GUI_Content.g.i.cs
+++ MyLeagueManagement/GUI/obj/Debug/GUI_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("burnley.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("zozd74mt2nl41.jpg")]
 
 
